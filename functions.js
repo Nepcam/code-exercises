03063a646875973a21ef2a34753a65aaecd847ab
Nepcam <cameron@aux.network>
2019-03-30 21:54:20 +1300

    function exercises

--- functions.js
+++ functions.js
@@ -13,9 +13,30 @@
 
 // module.exports = sayHello
 
-const constFunction = () => console.log("Hello there!")
+// const constFunction = () => console.log("Hello there!")
 
-constFunction
-//() => console.log("Hello there!")
-constFunction()
+// constFunction
+// //() => console.log("Hello there!")
+// constFunction()
+
+
+// I am a named function
+function greeting() {
+    console.log('Hi')
+}
+
+
+// I am a function without a name
+// I am an anonymous function
+function() {
+    console.log('Hi')
+}
+
+// I am a first class function
+// I am assigned to a variable
+let sayHi = function() {
+    console.log('Hi')
+}
+
+let sayHello = sayHi
 
